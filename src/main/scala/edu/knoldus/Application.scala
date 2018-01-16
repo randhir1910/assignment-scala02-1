4575187c7a0a2c70c6304b5110dc316c1a1c1944
randhir <randhiricadbg@gmail.com>
2018-01-16 16:14:26 +0530

    rotate improved

--- src/main/scala/edu/knoldus/Application.scala
+++ src/main/scala/edu/knoldus/Application.scala
@@ -3,13 +3,16 @@ package edu.knoldus
 import org.apache.log4j.Logger
 
 object Application extends App {
+
   val log = Logger.getLogger(this.getClass)
+
   val operation = new Operation
-  log.info(operation.rotateList(List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k), 3) + "\n")
+  val rotate = 3
+  log.info(operation.rotateList(List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k), rotate) + "\n")
   log.info(operation.compress(List('b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e, 'a, 'a, 'a, 'a), List.empty) + "\n")
 
-  val number1 = 19
-  val number2 = -10
+  val number1 = 44
+  val number2 = 70
   val number3 = 20
   val number4 = 7
 
--- src/main/scala/edu/knoldus/Operation.scala
+++ src/main/scala/edu/knoldus/Operation.scala
@@ -3,10 +3,12 @@ package edu.knoldus
 import org.apache.log4j.Logger
 
 class Operation {
+
   val log = Logger.getLogger(this.getClass)
 
   def rotateList(list: List[Symbol], length: Int): List[Symbol] = {
-    if (list.length >= length) {
+
+    if ( length > 0) {
       list match {
         case _ if length == 0 => list
         case head :: tail => rotateList(tail :+ head, length - 1)
@@ -16,32 +18,39 @@ class Operation {
       log.info("index size should be less than list size")
       list
     }
+
   }
 
   def happyNumber(number: Int, list: List[Int]): Boolean = {
-    def nextNumber(x: Int, next: Int): Int = {
-      x match {
-        case _ if x == 0 => next
-        case _ => val temp = next + (x % 10) * (x % 10)
-          nextNumber(x / 10, temp)
+
+    def nextNumber(num: Int, next: Int = 0): Int = {
+      num match {
+        case _ if num == 0 => next
+        case _ => val temp = next + (num % 10) * (num % 10)
+          nextNumber(num / 10, temp)
       }
+
     }
 
     list match {
+
       case _ if number < 0 => false
       case _ if number == 1 => true
       case head :: tail if tail.contains(number) => false
-      case _ => val num = nextNumber(number, 0)
+      case _ => val num = nextNumber(number)
         happyNumber(num, num :: list)
     }
+
   }
 
   def compress(list: List[Symbol], result: List[Symbol]): List[Symbol] = {
+
     list match {
-      case Nil => result.reverse
+      case Nil => result
       case head :: Nil => compress(Nil, head :: result)
       case head1 :: head2 :: tail if head1 == head2 => compress(head2 :: tail, result)
-      case head1 :: head2 :: tail if head1 != head2 => compress(head2 :: tail, head1 :: result)
+      case head1 :: head2 :: tail if head1 != head2 => compress(head2 :: tail, result :+ head1)
     }
   }
+
 }
